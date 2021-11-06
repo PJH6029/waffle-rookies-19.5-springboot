088826bbbc94eb03b3946b1cc0b5430e060b8a87
PJH6029 <top231902@naver.com>
2021-11-06 11:47:02 +0900

    Add test url

---
+++ seminar/src/main/kotlin/com/wafflestudio/seminar/global/common/api/LandingController.kt
@@ -0,0 +1,14 @@
+package com.wafflestudio.seminar.global.common.api
+
+import com.wafflestudio.seminar.domain.seminar.dto.SeminarDto
+import com.wafflestudio.seminar.global.common.dto.PingPongResponse
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("")
+class LandingController {
+    @GetMapping("/")
+    fun pingPong(): PingPongResponse {
+        return PingPongResponse("hi")
+    }
+}
---
+++ seminar/src/main/kotlin/com/wafflestudio/seminar/global/common/dto/PingPongResponse.kt
@@ -0,0 +1,5 @@
+package com.wafflestudio.seminar.global.common.dto
+
+data class PingPongResponse (
+    val value: String,
+)
